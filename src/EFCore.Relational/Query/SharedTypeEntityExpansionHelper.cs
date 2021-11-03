83128e2b707ba567c77ee7b3a1a96f028385a28b
maumar <maumar@microsoft.com>
2021-11-03 03:53:38 -0700

    Fix to #26451 - Temporal Table: Owned Entities support
    Added extensibility point to SharedTypeEntityExpandingExpressionVisitor so that we can create temporal table expressions representing owned entities, if their owner is also a temporal table expression.
    Just like any other nav expansion, this only works for AsOf operation.

---
+++ src/EFCore.Relational/Query/SharedTypeEntityExpansionHelper.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace Microsoft.EntityFrameworkCore.Query
+{
+    /// <inheritdoc/>
+    public class SharedTypeEntityExpansionHelper : ISharedTypeEntityExpansionHelper
+    {
+        /// <summary>
+        ///     Creates a new instance of the <see cref="SharedTypeEntityExpansionHelper" /> class.
+        /// </summary>
+        /// <param name="dependencies">Dependencies for this service.</param>
+        public SharedTypeEntityExpansionHelper(SharedTypeEntityExpansionHelperDependencies dependencies)
+        {
+            Dependencies = dependencies;
+        }
+
+        /// <summary>
+        ///     Dependencies for this service.
+        /// </summary>
+        protected virtual SharedTypeEntityExpansionHelperDependencies Dependencies { get; }
+
+        /// <inheritdoc/>
+        public virtual SelectExpression CreateInnerSelectExpression(
+            TableExpressionBase sourceTable,
+            IEntityType targetEntityType)
+            => Dependencies.SqlExpressionFactory.Select(targetEntityType);
+    }
+}
